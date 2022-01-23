754c136e2ab7bfae4d2ad6bb2a4a25891a1754c7
Nika Soltani Tehrani <nsoltanitehraniw@gmail.com>
2022-01-22 21:42:01 -0800

    chat version

--- Client.py
+++ Client.py
@@ -5,25 +5,30 @@ import threading
 class ClientNode:
     def __init__(self):
         self.node = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        port_and_ip = ('127.0.0.1', 12345)
+        port_and_ip = ('127.0.0.1', 1373)
         self.node.connect(port_and_ip)
+        self.connect = True
+        print("In case you want to subscribe to some topics please enter your request like: ")
+        print("host>port>subscribe>topic_1>topic_2>...>topic_n ")
+        print("In case you want to publish a message please enter your request like: ")
+        print("host>port>publish>topic>message")
 
-    def send_sms(self, SMS):
-        self.node.send(SMS.encode())
+    def send_message(self, message):
+        self.node.sendall(message.encode())
 
-    def receive_sms(self):
-        while True:
-            data = self.node.recv(1024).decode()
-            print(data)
+    def receive_message(self):
+        data = self.node.recv(1024).decode()
+        print(data)
 
     def main(self):
-        while True:
+        while self.connect:
             message = input()
-            self.send_sms(message)
+            self.send_message(message)
+            if message == "disconnect":
+                self.connect = False
 
 
 Client = ClientNode()
-always_receive = threading.Thread(target=Client.receive_sms)
-always_receive.daemon = True
+always_receive = threading.Thread(target=Client.receive_message)
 always_receive.start()
 Client.main()
--- Server.py
+++ Server.py
@@ -3,34 +3,107 @@ import threading
 
 
 class ServerNode:
-    
+
     def __init__(self):
-        print("server is on.")
-        self.PORT = 12345
-        self.IP = '127.0.0.1'
-        self.node = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        port_and_ip = (self.IP, self.PORT)
-        self.node.bind(port_and_ip)
-        print(f"server is listening on port {self.PORT}")
-        self.node.listen(5)
-        self.connection, addr = self.node.accept()
+        self.list_of_clients = []
+        self.topics_dict = dict()
+        self.PORT = 1373
+        self.HOST = '127.0.0.1'
+        self.accept_connections()
 
-    def send_sms(self, SMS):
-        self.connection.send(SMS.encode())
+    def accept_connections(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.server_socket:
+            print("Server is on.")
+            self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.server_socket.bind((self.HOST, self.PORT))
+            self.server_socket.listen(100)
+            print(f"Server is listening on port {self.PORT}")
+            while True:
+                conn, address = self.server_socket.accept()
+                self.list_of_clients.append(conn)
+                threading.Thread(target=self.handler, args=(conn, address)).start()
 
-    def receive_sms(self):
-        while True:
-            data = self.connection.recv(1024).decode()
-            print(data)
+    @staticmethod
+    def send_message(message, connection):
+        connection.sendall(message.encode())
 
-    def main(self):
-        while True:
-            message = input()
-            self.send_sms(message)
+    @staticmethod
+    def receive_message(connection):
+        # while True:
+        data = connection.recv(1024).decode()
+        print(data)
+        data_list = data.split(">")
+        return data, data_list
+
+    def publish(self, topic, conn):
+        if topic in self.topics_dict.keys():
+            self.send_pub_ack(conn)
+            return
+        else:
+            new_topic = {topic: []}
+            self.topics_dict.update(new_topic)
+            print("New topic with the concept of " + topic + " added.")
+            self.send_pub_ack(conn)
+
+    def send_pub_ack(self, connection):
+        pub_ack_msg = "your message published successfully"
+        self.send_message(pub_ack_msg, connection)
+
+    def broadcast(self, topic, topic_msg, conn):
+        subscribed_clients = None
+        message = topic + ": " + topic_msg
+        # print(topic)
+        # print(self.topics_dict[topic])
+        if self.topics_dict[topic] is not None:
+            for topic_key in self.topics_dict.keys():
+                if topic_key == topic:
+                    subscribed_clients = self.topics_dict[topic]
+                    break
+            for connection in subscribed_clients:
+                if connection != conn:
+                    connection.sendall(message.encode())
+
+    def subscribe(self, conn, topic_list):
+        successful_subscriptions = []
+        failed_subscriptions = []
+        for topic in topic_list:
+            if topic in self.topics_dict.keys():
+                new_conns = self.topics_dict[topic]
+                new_conns.append(conn)
+                new_topic = {topic: new_conns}
+                self.topics_dict.update(new_topic)
+                successful_subscriptions.append(topic)
+            else:
+                failed_subscriptions.append(topic)
+            self.send_sub_ack(conn, successful_subscriptions, failed_subscriptions)
+
+    def send_sub_ack(self, connection, successful_topic_list, failed_topic_list):
+        p_sub_ack_msg = "subscribing on: "
+        n_sub_ack_msg = "subscription failed for"
+        message = p_sub_ack_msg, successful_topic_list, "and", n_sub_ack_msg, failed_topic_list
+        self.send_message(message, connection)
+
+    def handler(self, conn, address):
+        with conn:
+            print("Connected by", address)
+            while True:
+                data, data_list = self.receive_message(conn)
+                if not data:
+                    break
+                if data == "disconnect":
+                    break
+                if data_list[2] == "publish":
+                    self.publish(data_list[3], conn)
+                    self.broadcast(data_list[3], data_list[4], conn)
+                if data_list[2] == "subscribe":
+                    self.subscribe(conn, data_list[3:])
+                else:
+                    self.send_message("You entered wrong request", conn)
+            self.remove_client(conn)
+            print("Disconnected by", address)
+
+    def remove_client(self, conn):
+        self.list_of_clients.remove(conn)
 
 
 server = ServerNode()
-always_receive = threading.Thread(target=server.receive_sms)
-always_receive.daemon = True
-always_receive.start()
-server.main()
